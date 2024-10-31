d3080504c95aef6bebe74d70a78d45a21438a94c
Maksym Dombrov <maksymdombrov@gmail.com>
2024-10-31 19:43:20 +0200

    create serverless function

--- lambda/main.go
+++ lambda/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+type MyEvent struct {
+	Username string `json:"username"`
+}
+
+// * Take in a payload and do something with it
+func HandleRequest(event MyEvent) (string, error) {
+	if event.Username == "" {
+		return "", fmt.Errorf("username is empty")
+	}
+
+	return fmt.Sprintf("Successfully called by - %s", event.Username), nil
+}
+
 func main() {
 
 }
