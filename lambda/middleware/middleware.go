ab8e620a01a539a9fed839166cd0d5e5a8637c68
Maksym Dombrov <maksymdombrov@gmail.com>
2024-11-01 12:14:11 +0200

    add middleware

---
+++ lambda/middleware/middleware.go
@@ -0,0 +1,86 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+//* Extract the request headers
+//* Extract claims
+//* Validate everything
+
+func ValidateJWTMiddleWare(next func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)) func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+		//extract the token from the request headers
+		tokenString := extractTokenFromHeaders(request.Headers)
+		if tokenString == "" {
+			return events.APIGatewayProxyResponse{
+				Body:       "Missing Auth Token",
+				StatusCode: http.StatusUnauthorized,
+			}, nil
+		}
+
+		//parse the token for the claims
+		claims, err := parseToken(tokenString)
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				Body:       "User not authorized",
+				StatusCode: http.StatusUnauthorized,
+			}, nil
+		}
+
+		expires := int64(claims["expires"].(float64))
+		if time.Now().Unix() > expires {
+			return events.APIGatewayProxyResponse{
+				Body:       "Token expired",
+				StatusCode: http.StatusUnauthorized,
+			}, nil
+		}
+
+		return next(request)
+
+	}
+
+}
+
+func extractTokenFromHeaders(headers map[string]string) string {
+	authorizationHeader, ok := headers["Authorization"]
+	if !ok {
+		return ""
+	}
+
+	splitToken := strings.Split(authorizationHeader, "Bearer ") //make sure to include space after Bearer
+	if len(splitToken) != 2 {
+		return ""
+	}
+
+	return splitToken[1]
+}
+
+func parseToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		secret := "secret" //should match the secret in types.go
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("unauthorized")
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("token is not valid - unauthorized")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("claims of unauthorized type")
+	}
+
+	return claims, nil
+}
