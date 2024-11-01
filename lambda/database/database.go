0b78ac2eff39dbf1a53d3500741372d4286813de
Maksym Dombrov <maksymdombrov@gmail.com>
2024-11-01 10:48:57 +0200

    add getuser fn for login

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,11 +1,13 @@
 package database
 
 import (
+	"fmt"
 	"lambda-func/types"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 const (
@@ -73,4 +75,33 @@ func (u DynamoDBClient) InsertUser(user types.RegisterUser) error {
 	}
 
 	return nil
+}
+
+func (u DynamoDBClient) GetUser(username string) (types.User, error) {
+	var user types.User
+
+	result, err := u.dataBaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(username),
+			},
+		},
+	})
+	if err != nil {
+		return user, err
+	}
+
+	//* user does not exist
+	if result.Item == nil {
+		return user, fmt.Errorf("user not found")
+	}
+
+	//* unmarshal the item
+	err = dynamodbattribute.UnmarshalMap(result.Item, &user)
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
 }
