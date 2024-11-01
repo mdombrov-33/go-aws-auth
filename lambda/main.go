f7a9e001b6375a50be48d33e3d29672d515b1292
Maksym Dombrov <maksymdombrov@gmail.com>
2024-11-01 11:06:35 +0200

    refactor registerUserHandler

--- lambda/main.go
+++ lambda/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"lambda-func/app"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events" // allows us to extract paths, requests etc.
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -22,5 +24,17 @@ func HandleRequest(event MyEvent) (string, error) {
 
 func main() {
 	lambdaApp := app.NewApp()
-	lambda.Start(lambdaApp.APIHandler.RegisterUserHandler)
+	//* Hook lambda function to the gateway
+	lambda.Start(func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		switch request.Path {
+		case "/register":
+			return lambdaApp.APIHandler.RegisterUserHandler(request)
+		default:
+			return events.APIGatewayProxyResponse{
+				Body:       "Not Found",
+				StatusCode: http.StatusNotFound,
+			}, nil
+		}
+	})
+
 }
